0997d9446324ddd34b2ceac5335ce8e71c5a741c
Lilmaniacloco <maradiaga.derick@gmal.com>
2024-11-15 18:12:27 -0800

    added login

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { LoginRequest } from './login-request';
+import { LoginResponse } from './login-response';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+
+  login(loginRequest:LoginRequest): Observable <LoginResponse>{
+    return this.http.post<LoginResponse>(`${environment.baseUrl}api/Admin/LogIn`,
+      loginRequest);
+   
+
+
+  }
+
+}
